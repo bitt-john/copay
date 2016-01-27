50357ec177826c737a700771dcd277211d78329c
bitt <john@bitt.com>
2016-01-26 22:35:26 -0400

    url ssl

--- src/js/controllers/preferencesBwsUrl.js
+++ src/js/controllers/preferencesBwsUrl.js
@@ -25,7 +25,7 @@ angular.module('copayApp.controllers').controller('preferencesBwsUrlController',
           break;
         case 'loc':
         case 'local':
-          bws = 'http://localhost:3232/bws/api'
+          bws = 'https://localhost:3232/bws/api'
           break;
       };
       if (bws) {
--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -12,7 +12,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
     // Bitcore wallet service URL
     bws: {
-      url: 'http://localhost:3232/bws/api',
+      url: 'https://localhost:3232/bws/api',
     },
 
     // wallet default config
